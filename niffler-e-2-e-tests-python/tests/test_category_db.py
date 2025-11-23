de8e920122ba20e900de83f1151f62e9dc2e4fe0
Dimitri Maksimov <salesakk@gmail.com>
2025-11-23 17:51:41 +0300

    add database

--- niffler-e-2-e-tests-python/tests/test_category_db.py
+++ niffler-e-2-e-tests-python/tests/test_category_db.py
@@ -1,8 +1,6 @@
 from marks import TestData, Pages
 from models.category import CategoryAdd
 from models.spend import SpendAdd
-import pytest
-
 
 # тест крашит все остальные
 
@@ -10,14 +8,13 @@ import pytest
 TEST_CATEGORY = "school123456"
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @Pages.main_page_late
 @TestData.category(TEST_CATEGORY)
 @TestData.spends(SpendAdd(category=CategoryAdd(name=TEST_CATEGORY)))
 def test_check_category_in_db(category, spends, spend_db, envs):
     username = envs.test_username
     spend_from_db = spend_db.get_user_categories(username)
-    print(spend_from_db)
 
     first_category = spend_from_db[0]
     assert first_category.username == username
